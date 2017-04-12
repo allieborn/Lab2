94ceeb3ae7bc9c58d865beaac86525e5e72de5c8
Alexandra <aborn601722@Alexandras-MacBook-Pro.local>
2017-04-11 21:36:09 -0400

    initial commit

---
+++ src/com/InClassLabs/Main.java
@@ -0,0 +1,39 @@
+package com.InClassLabs;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Enter a number between 1 and 100:");
+        Scanner scan = new Scanner(System.in);
+        int userNum = scan.nextInt();
+        if (userNum >= 1 && userNum < 101) {
+            if (userNum % 2 != 0 && userNum < 60) {
+                System.out.println(userNum + " odd");
+            }
+            if (userNum % 2 == 0 && userNum < 25) {
+                System.out.println("Even and less than 25");
+            }
+            if (userNum % 2 == 0 && 26 < userNum && userNum < 60) {
+                //is there a way to combine this range?
+                System.out.println("Even");
+            }
+            if (userNum % 2 == 0 && userNum > 60) {
+                System.out.println(userNum + "even");
+            }
+            if (userNum % 2 != 0 && userNum > 60) {
+                System.out.println("Odd and over 60");
+            }
+            //would there be a benefit to using 'else if' rather than just 'if'?
+
+
+        }
+        else {
+            System.out.println("You did not enter a valid number. Please re-enter a valid number between 1 and 100:");
+            //is there a way to loop this back to repeat plan? 
+        }
+
+    }
+}
